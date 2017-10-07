20b507845dd7a7a9e174ea46732a115f1a7a7123
norom <norom@yandex.ru>
2017-10-07 21:11:23 +0300

    Add files via upload

---
+++ ConvertToBin.cpp
@@ -0,0 +1,104 @@
+#include "ConvertToBin.h"
+
+Data::Data(std::string ticket, float period, std::vector<Row>& data)
+{
+	m_period = period;
+	m_ticket = ticket;
+	m_data = std::move(data);
+}
+
+void Utils::ConverToBinary(std::string fileIn, std::string fileOut)
+{
+	Data data;
+	ReadFromTxt(fileIn, fileOut, data);
+
+	std::ofstream streamFileOut(fileOut, std::ios::binary | std::ios::out);
+	streamFileOut << data;
+	streamFileOut.close();
+};
+
+void Utils::ReadFromTxt(std::string fileIn, std::string fileOut, Data& o_data)
+{
+	std::ifstream streamFileIn(fileIn);
+	std::string line;
+	std::getline(streamFileIn, line);//skip headers
+	bool headersReaded = false;
+	uint64_t numberOfRow = 0;
+	while (!streamFileIn.eof())
+	{
+		std::getline(streamFileIn, line);
+		std::istringstream ss(line);
+		int valueIndex = 0;
+		Data::Row row;
+		std::string value;
+		if (!headersReaded)
+		{
+			std::getline(ss, value, ',');
+			o_data.m_ticket = value;
+			std::getline(ss, value, ',');
+			o_data.m_period = std::atof(value.c_str());
+			headersReaded = true;
+		}
+		else
+		{
+			std::getline(ss, value, ',');
+			std::getline(ss, value, ',');
+		}
+
+		std::getline(ss, value, ',');
+		row.date = std::atoi(value.c_str());
+
+		std::getline(ss, value, ',');
+		row.time = std::atoi(value.c_str());
+
+		std::getline(ss, value, ',');
+		row.open = std::atof(value.c_str());
+
+		std::getline(ss, value, ',');
+		row.high = std::atof(value.c_str());
+
+		std::getline(ss, value, ',');
+		row.low = std::atof(value.c_str());
+
+		std::getline(ss, value, ',');
+		row.close = std::atof(value.c_str());
+
+		std::getline(ss, value, ',');
+		row.vol = std::atoi(value.c_str());
+
+		o_data.m_data.push_back(row);
+		numberOfRow++;
+	}
+	std::cout << "Number of readed rows is " << numberOfRow << '\n';
+};
+
+
+#include <ctime>
+//Main just for the tests
+int main(int argc, char **argv)
+{
+	std::string fileIn = "input2.txt";
+	std::string fileOut = "output.bin";
+	Utils::ConverToBinary(fileIn, fileOut);
+	
+	{
+		std::clock_t begin = clock();
+		std::ifstream streamFileCheck("output.bin", std::ios::binary | std::ios::in);
+		Data dataCheck;
+		streamFileCheck >> dataCheck;
+		std::clock_t end = clock();
+		double elapsed_secs = double(end - begin) / CLOCKS_PER_SEC;
+		std::cout << "Bin read clock time: " << elapsed_secs << '\n';
+	}
+
+	{
+		std::clock_t begin = clock();
+		Data data;
+		Utils::ReadFromTxt(fileIn, fileOut, data);
+		std::clock_t end = clock();
+		double elapsed_secs = double(end - begin) / CLOCKS_PER_SEC;
+		std::cout << "Text clock time: " << elapsed_secs << '\n';
+	}
+	return 0;
+
+}
---
+++ ConvertToBin.h
@@ -0,0 +1,93 @@
+#pragma once
+#include <stdio.h>
+#include <assert.h>
+#include <chrono>
+#include <iostream>
+#include <fstream>
+#include <string>
+#include <sstream>
+#include <memory>
+#include <cstdint>
+#include <vector>
+
+
+
+struct Data
+{
+	struct Row
+	{
+		uint32_t date;
+		uint32_t time;
+		float open;
+		float high;
+		float low;
+		float close;
+		uint32_t vol;
+
+	};
+
+	static uint8_t GetVersion() { return 1; }
+	static uint8_t GetMagicNumber() { return 0x44a6; }
+
+	Data() :m_period(0.f) {};
+	Data(std::string ticket, float period, std::vector<Row>& data);
+	std::size_t GetSizeInBytes() const { return m_data.size() * sizeof(Row); }
+
+
+	std::string m_ticket;
+	float m_period;
+	std::vector<Row> m_data;
+};
+namespace Utils
+{
+	void ConverToBinary(std::string fileIn, std::string fileOut);
+	void ReadFromTxt(std::string fileIn, std::string fileOut, Data& o_data);
+};
+
+
+
+std::ostream& operator<<(std::ostream& os, const Data& obj)
+{
+	uint8_t mn = Data::GetMagicNumber();
+	uint8_t ver = Data::GetVersion();
+	size_t size = obj.GetSizeInBytes();
+
+	os.write(reinterpret_cast<const char*>(&mn), sizeof(uint8_t));
+	os.write(reinterpret_cast<const char*>(&ver), sizeof(uint8_t));
+	size_t strSize = obj.m_ticket.size();
+	os.write(reinterpret_cast<const char*>(&strSize), sizeof(size_t));
+	os.write(obj.m_ticket.c_str(), strSize);
+	os.write(reinterpret_cast<const char*>(&obj.m_period), sizeof(float));
+	os.write(reinterpret_cast<const char*>(&size), sizeof(size_t));
+	os.write(reinterpret_cast<const char*>(&obj.m_data[0]), obj.GetSizeInBytes());
+
+	return os;
+}
+
+std::istream& operator >> (std::istream& is, Data& obj)
+{
+	uint8_t maginNumber;
+	uint8_t version;
+	is.read(reinterpret_cast<char*>(&maginNumber), sizeof(uint8_t));
+	is.read(reinterpret_cast<char*>(&version), sizeof(uint8_t));
+	if (maginNumber != Data::GetMagicNumber())
+		throw std::exception("Invalid magic number of file");
+	if (version != Data::GetVersion())
+	{
+		std::stringstream ss;
+		ss << "Versions is differents " << version << "; " << Data::GetVersion();
+		throw std::exception("Versions is differents");
+	}
+	size_t strSize = 0;
+	is.read(reinterpret_cast< char*>(&strSize), sizeof(size_t));
+	obj.m_ticket.resize(strSize);
+	is.read(&obj.m_ticket[0], strSize);
+	is.read(reinterpret_cast<char*>(&obj.m_period), sizeof(float));
+	size_t size;
+	is.read(reinterpret_cast<char*>(&size), sizeof(size_t));
+	obj.m_data.resize(size);
+	is.read(reinterpret_cast<char*>(&obj.m_data[0]), obj.GetSizeInBytes());
+
+	return is;
+}
+
